a27097853578f1537aa23ca4889e46bc49372c8b
Tyler Cunningham <tyler@cyberchimps.com>
2012-05-07 20:38:31 -0700

    categories

--- core/actions/recent-posts-actions.php
+++ core/actions/recent-posts-actions.php
@@ -5,22 +5,20 @@ add_action( 'business_recent_posts_element', 'business_recent_posts_element_cont
 function business_recent_posts_element_content() {
 
 	global $wp_query;
-	$args = array_merge( $wp_query->query, array( 'showposts' => 4 ) );
+	$args = array_merge( $wp_query->query, array( 'showposts' => 4, 'ignore_sticky_posts' => 1  ));
 	query_posts( $args );
 ?>
 <div class="container">
 <div class="row">
-	<h3>Recent Blog Posts</h3>
+	<h4 style="color:white;">Recent Blog Posts</h4><br />
 	<div id="recent_posts_wrap">
-	
-
 	
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 	
 		<div class="three columns">
 		
 			<h5><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h5>
-			<?php the_time( 'd/m/y');?>
+			<h6 style="color:white;"><?php the_time( 'd/m/y');?> - <?php the_category(', ') ?> - <?php comments_popup_link( __('No Comments', 'business' ), __('1 Comment', 'business' ), __('% Comments' , 'business' )); //need a filer here ?></h6>
 			<?php
 				if ( has_post_thumbnail()) {
  		 			echo '<div class="featured-image">';
