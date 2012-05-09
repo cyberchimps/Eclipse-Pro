117698a8032ea4755a2af1246186c7f126e337b6
Trent Lapinski <cyberchimpspro@gmail.com>
2012-05-09 16:24:07 -0700

    twitter bird

--- core/actions/twitterbar-actions.php
+++ core/actions/twitterbar-actions.php
@@ -55,13 +55,16 @@ function response_display_latest_tweets( $username, $show_replies = 0 ) {
 ?>
 	<div class="row">
 		<div id="twitterbar" class="twelve columns"><!--id="twitterbar"-->
+			<div id="bird">
+				 <img src="'.get_template_directory_uri().'/images/twitterbird.png" />
+			</div>
 			<div id="twittertext">
 				<?php
 					if ( $latest_tweet ) {	
 						$screen_name = $latest_tweet['user']['screen_name'];
 						$user_permalink = 'http://twitter.com/#!/'.$screen_name;
 						$tweet_permalink = 'http://twitter.com/#!/'.$screen_name.'/status/'.$latest_tweet['id_str'];
-						echo '<a href="'.$user_permalink.'"> <img src="'.get_template_directory_uri().'/images/twitterbird.png" class="bird" /> '. $screen_name .' - </a>'.$latest_tweet['text'].' <small><a href="'.$tweet_permalink.'">' .human_time_diff(strtotime($latest_tweet['created_at']), current_time('timestamp')).' ago</a></small>';
+						echo '<a href="'.$user_permalink.'"> '. $screen_name .' - </a>'.$latest_tweet['text'].' <small><a href="'.$tweet_permalink.'">' .human_time_diff(strtotime($latest_tweet['created_at']), current_time('timestamp')).' ago</a></small>';
 					} else {
 						echo '<p>No tweets to display</p>';
 					}
