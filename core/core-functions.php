b71bdc39aa215d9e2dc677c65f875c8113de527c
Tyler Cunningham <tyler@cyberchimps.com>
2012-05-09 14:41:46 -0700

    kind of getting there

--- core/core-functions.php
+++ core/core-functions.php
@@ -104,20 +104,21 @@ function response_comment($comment, $args, $depth) {
      <div id="comment-<?php comment_ID(); ?>">
       <div class="comment-author vcard">
          <?php echo get_avatar( $comment, 48 ); ?>
-
-         <?php printf(__('<cite class="fn">%s</cite> <span class="says"></span>'), get_comment_author_link()) ?>
       </div>
-      
-     <div class="comment-meta commentmetadata"><a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ) ?>"><?php printf(__('%1$s at %2$s', 'core' ), get_comment_date(),  get_comment_time()) ?></a><?php edit_comment_link(__('(Edit)', 'core' ),'  ','') ?> | <span class="reply"> <?php comment_reply_link(array_merge( $args, array('depth' => $depth, 'max_depth' => $args['max_depth']))) ?></span></div>
+     <div style="background-color: green; margin-left: 100px;">
+     <div class="comment-meta commentmetadata">     <span style="float:left"><?php printf(__('<cite class="fn">%s</cite> <span class="says"></span>'), get_comment_author_link()) ?></span><a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ) ?>"><?php printf(__('%1$s at %2$s', 'core' ), get_comment_date(),  get_comment_time()) ?></a><?php edit_comment_link(__('(Edit)', 'core' ),'  ','') ?> | <span class="reply"> <?php comment_reply_link(array_merge( $args, array('depth' => $depth, 'max_depth' => $args['max_depth']))) ?></span>
+     </div>
       
       <?php if ($comment->comment_approved == '0') : ?>
          <em><?php _e('Your comment is awaiting moderation.', 'core' ) ?></em>
          <br />
       <?php endif; ?>
-
+		
+		<div class="comment_wrap">	
       <?php comment_text() ?>
+      </div>
 
-     </div>
+     </div></div>
 <?php
 }
 
