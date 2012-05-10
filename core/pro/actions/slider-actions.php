e825d4fb09a5c22b89cd074ba0795567e3788c7b
Tyler Cunningham <tyler@cyberchimps.com>
2012-05-09 17:35:43 -0700

    responsive slider height thing

--- core/pro/actions/slider-actions.php
+++ core/pro/actions/slider-actions.php
@@ -354,7 +354,7 @@ To create a Custom Slide please go to the Custom Slides tab in WP-Admin. Once yo
 <!-- Apply slider CSS based on user settings -->
 
 	<style type="text/css" media="screen">
-		#orbitDemo { max-height: <?php echo $height ?>px !important; }
+		
 		#slider { width: <?php echo $csWidth ?>px; height: <?php echo $height ?>px; margin: auto; }
 	</style>
 
