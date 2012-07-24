3ea0ad843c57816e04aa4950a9eab759641ea27c
hugosaner <hugo@sanerdesign.com>
2012-07-24 09:07:22 +0100

    options page link to documentation was going to ifeature
    Signed-off-by: hugosaner <hugo@sanerdesign.com>

--- core/classy-options/classy-options-framework/classy-options.php
+++ core/classy-options/classy-options-framework/classy-options.php
@@ -101,7 +101,7 @@ class ClassyOptions {
 					<ul class="buttons">
 						<li><a href="http://cyberchimps.com/store/" target="_blank"><?php printf( __( 'CyberChimps Store', 'ifeature' )); ?></a></li>
 						<li><a href="http://cyberchimps.com/support" target="_blank"><?php printf( __( 'Support', 'ifeature' )); ?></a></li>
-						<li><a href="http://cyberchimps.com/ifeaturepro/docs/" target="_blank"> <?php printf( __( 'Documentation', 'ifeature' )); ?></a></li>
+						<li><a href="http://cyberchimps.com/eclipsepro/docs/" target="_blank"> <?php printf( __( 'Documentation', 'ifeature' )); ?></a></li>
 						<li><a href="http://cyberchimps.com/forum/" target="_blank"><?php printf( __( 'Forum', 'ifeature' )); ?></a></li>
 						<li><a href="http://twitter.com/#!/cyberchimps" target="_blank"><?php printf( __( 'Twitter', 'ifeature' ));?></a></li>
 						<li><a href="http://facebook.com/cyberchimps/" target="_blank"><?php printf( __( 'Facebook', 'ifeature' ));?></a></li>
