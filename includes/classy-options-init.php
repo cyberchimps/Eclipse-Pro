b80111d2961e49703a24709422caf69d92e76ac2
hugosaner <hugo@sanerdesign.com>
2012-08-21 11:52:01 +0100

    add custom url to logo and fixed fallback if no url is in custom logo. There is no default image
    Signed-off-by: hugosaner <hugo@sanerdesign.com>

--- includes/classy-options-init.php
+++ includes/classy-options-init.php
@@ -95,6 +95,7 @@ $options
 			->subsection("Header Options")
 			->checkbox($themeslug."_custom_logo", "Custom Logo")
 			->upload($themeslug."_logo", "Logo")
+			->text($themeslug."_logo_url", "Logo Custom URL", array('default' => home_url()))
 			->upload($themeslug."_favicon", "Custom Favicon")
 			->upload($themeslug."_apple_touch", "Apple Touch Icon", array('default' => array('url' => TEMPLATE_URL . '/images/apple-icon.png')))
 		->subsection_end()
