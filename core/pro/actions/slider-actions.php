834f30253175c2f0f97f7de7bbefb8709cb91eb5
mfssurya <suryap@mindfiresolutions.com>
2012-10-16 14:05:41 +0530

    Reverted slider fix.

--- core/pro/actions/slider-actions.php
+++ core/pro/actions/slider-actions.php
@@ -207,10 +207,8 @@ function response_slider_content() {
 		the_post(); 
 
 	    	/* Post-specific variables */	
-				
-				$post_thumbnail_id = get_post_thumbnail_id( $post->ID );
-				$post_image = ( $usecustomslides != 'custom' OR $type != '0' ) ? wp_get_attachment_image_src( $post_thumbnail_id, 'full' ) : '';
-	    	$customimage 		= ( $usecustomslides == 'custom' OR $type == '0' ) ? get_post_meta($post->ID, $themeslug.'_slider_image' , true) : $post_image[0]; /* Gets slide custom image from page/post meta option */
+
+			$customimage 		= get_post_meta($post->ID, $themeslug.'_slider_image' , true);  /* Gets slide custom image from page/post meta option */
 	    	$customtext 		= get_post_meta($post->ID, $themeslug.'_slider_caption' , true);
 	    	$customlink 		= get_post_meta($post->ID, $themeslug.'_slider_url' , true); /* Gets link from custom slide meta option */
 	    	$permalink 			= get_permalink(); /* Gets post URL for blog post slides */
